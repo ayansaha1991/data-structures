112eae340af8193454d43c128ece2cb96aadd8c1
ayansaha1991 <ayansaha1991@gmail.com>
2017-08-07 16:44:18 +0530

    LinkedList initial setups

---
+++ src/LinkedList/MyLinkedList.java
@@ -0,0 +1,81 @@
+package LinkedList;
+
+public class MyLinkedList<E> {
+	
+	private MyNode<E> head = null;
+	
+	private int size = 0;
+	
+	public void addFirst(E data) {
+
+		MyNode<E> temp = head;
+		final MyNode<E> newNode = new MyNode<E>(data, temp);
+		newNode.data = data;
+		newNode.next = head;
+		head = newNode;
+	}
+	
+//	public void add(E data);
+	
+	public void addAt(E data, int pos) {
+		
+		MyNode<E> temp = head;
+		
+		int count = 1;
+		while(count < pos) {
+			temp = temp.next;
+			System.out.println("Inside travarse : " + temp.data);
+			count++;
+		}
+		
+		MyNode<E> newNode = new MyNode<E>(data, temp.next);
+		temp.next = newNode;
+		
+	}
+	
+	
+	
+//	public E remove();
+	
+//	public E remove(int pos);
+
+	public void printList() {
+		
+		MyNode<E> temp = head;
+		while(temp != null) {
+			System.out.print(temp.data + ", " );
+			temp = temp.next;
+		}
+	}
+	
+	private static final class MyNode<E> {
+		
+		private E data;
+		private MyNode<E> next;
+		
+		public MyNode(E data, MyNode<E> next) {
+			this.data = data;
+			this.next = next;
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is compile time");
+		
+		MyLinkedList<Integer> mylist = new MyLinkedList<>();
+		mylist.addFirst(10);
+		mylist.addFirst(11);
+		mylist.addFirst(13);
+		mylist.addFirst(14);
+		mylist.printList();
+		
+		mylist.addAt(12, 2);
+		mylist.printList();
+		mylist.addAt(12, 2);
+		
+		mylist.printList();
+	}
+
+}
