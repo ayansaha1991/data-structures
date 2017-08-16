68fe271df670b01866891455a35290482e3eecb9
ayansaha1991 <ayansaha1991@gmail.com>
2017-08-16 23:09:36 +0530

    BstNode as of 16-08-2017

--- src/Tree/BSTTree.java
+++ src/Tree/BSTTree.java
@@ -1,29 +1,63 @@
 package Tree;
 
-public class BSTTree<E> {
+public class BSTTree {
 	
-	private BSTNode<E> root;
+	private BSTNode root;
 	
 	public BSTTree() {
 		root = null;
 	}
 	
-	private static class BSTNode<E> {
+	private static class BSTNode {
 		
-		E data;
-		BSTNode<E> left;
-		BSTNode<E> right;
+		Integer data;
+		BSTNode left;
+		BSTNode right;
 		
-		public BSTNode(E data) {
+		public BSTNode(Integer data) {
 			this.data = data;
 			this.left = null;
 			this.right = null;
 		}
 	}
 	
-	public void insert(E data) {
+	public void printInOrder(BSTNode node) {
+
+		if(node == null) {
+			return;
+		}
+
+		printInOrder(node.left);
+		System.out.println(node.data + ", ");
+		printInOrder(node.right);
+	}
+
+	
+	private BSTNode createNode(Integer data) {
 		
-		BSTNode<E> newNode = new BSTNode(data);
+		BSTNode newNode = new BSTNode(data);
+		return newNode;
+	}
+	
+	public void insert(BSTNode current ,Integer data) {
+		
+		BSTNode newNode = createNode(data);
+		
+		if (root == null) {
+			root = newNode;
+		}
+		
+		if(newNode.data <= root.left.data) {
+			insert(current.left, data);
+		} else {
+			insert(current.right, data);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		BSTTree tree = new BSTTree();
 	}
 
 }
